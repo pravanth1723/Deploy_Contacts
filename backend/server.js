b6fcaa2d869378e630aa9215be4e0f3362508f38
pravanth1723 <144945561+pravanth1723@users.noreply.github.com>
2024-10-13 14:36:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config();
 const connectDb = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');

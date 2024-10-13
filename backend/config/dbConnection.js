cf8a92bf85705833f1efed5e003c9faa706c9825
pravanth1723 <144945561+pravanth1723@users.noreply.github.com>
2024-10-13 14:35:05 +0530

    Update dbConnection.js

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const connectDb=async()=>{
     try{
-        const connect=await mongoose.connect(process.env.CONNECTIONSTRING)
+        const connect=await mongoose.connect('mongodb+srv://Admin:Mongodb1723@pravanthcluster.9qcjv24.mongodb.net/contactsBackend?retryWrites=true&w=majority&appName=pravanthCluster')
         console.log('DB connected',
             connect.connection.host,
             connect.connection.name
